8693fd01e23ea9b464f01efe3e10fb78922f9ca8
Justin Russell <Justin@justins-macbook.lan>
2020-09-24 12:47:39 -0700

    added day 2 questions

---
+++ day2.py
@@ -0,0 +1,11 @@
+# two sum
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_dict = dict()
+        
+        for i in range(len(nums)):
+            second = target - nums[i]
+            if second in nums_dict:
+                return [nums_dict[second], i]
+            nums_dict[nums[i]] = i
